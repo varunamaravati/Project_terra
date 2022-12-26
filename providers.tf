1f62aee73d27ee4c26b3bfebafb581381d844bde
V07 <amaravativarun@gmail.com>
2022-12-26 16:33:42 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -11,4 +11,8 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id   = "855b3c2f-f781-4828-a396-d5749d577e21"
+  tenant_id       = "ca84daf0-3cd3-4718-bd17-bd6bdb5f1cf9"
+  client_id       = "8a30b0c8-d1f1-4944-b0d4-68d2261a6766"
+  client_secret   = "UcB8Q~Rp3KOTJZfgaP7dQVgkLRsckAYQakjKVceM"
 }
